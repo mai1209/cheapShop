6fdecb54e66aaed6ea8b2152bc6fd3547f6970ee
Mai <maidev@Mac.fibertel.com.ar>
2025-11-18 16:22:31 -0300

    deployd veercel

--- frontend/darko/vite.config.js
+++ frontend/darko/vite.config.js
@@ -1,22 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: "dist"
   },
-  base: "/",
-  server: {
-    host: true,
-    allowedHosts: [
-      "cheap-shop-two.vercel.app"
-    ],
-  },
-  preview: {
-    host: true,
-    allowedHosts: [
-      "cheap-shop-two.vercel.app"
-    ],
-  },
+  base: "/"
 });
